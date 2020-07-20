3761dfff55bad5c522018e18edc0e5e6a602cb6c
Alan Wrigglesworth <61271711+AlanXVII@users.noreply.github.com>
2020-07-20 18:48:09 +0200

    Create Person.cs

---
+++ Play/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Play
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi  {0}, I am {1}", to, Name);
+        }
+
+        public static Person Parse(string str) // declare a parse method as a class so that we don't have to create a person object before we can call the method
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+}
