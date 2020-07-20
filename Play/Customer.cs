0d05ccb3841abeea8d153e8623d33b4062d0d013
Alan Wrigglesworth <61271711+AlanXVII@users.noreply.github.com>
2020-07-20 18:47:52 +0200

    Create Customer.cs

---
+++ Play/Customer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Play
+{
+    public class Customer
+    {
+        public int Id;
+        public string Name;
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(string name, int id)
+        {
+            this.Name = name;
+            this.Id = id;
+        }
+
+        public Customer(string name)
+        {
+            this.Name = name;
+        }
+
+        public Customer()
+        {
+
+        }
+    }
+}
