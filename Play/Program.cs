2fad3b0dc0553d30950924a88ed237e8f27afd12
Alan Wrigglesworth <61271711+AlanXVII@users.noreply.github.com>
2020-07-20 18:48:22 +0200

    Create Program.cs

---
+++ Play/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Play
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = Person.Parse("John");
+            person.Introduce("Alan");
+
+            var customer = new Customer();
+            Console.WriteLine(customer.Id);
+            Console.WriteLine(customer.Name);
+        }
+    }
+}
