7fb2645ad4c43569f4f116b07c809e12f8abab8e
Alan Wrigglesworth <61271711+AlanXVII@users.noreply.github.com>
2020-07-26 19:51:26 +0200

    Create MyStopWatch.cs

---
+++ ExerciseOne/MyStopWatch.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExerciseOne
+{
+    class MyStopWatch
+    {
+        public DateTime StartTime;
+        public DateTime StopTime;
+        public TimeSpan TimeElapsed;
+
+        public MyStopWatch()
+        {
+
+        }
+
+        public void Start()
+        {
+            StartTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            StopTime = DateTime.Now;
+        }
+
+        public TimeSpan Elapsed()
+        {
+            TimeElapsed = StopTime - StartTime;
+            return TimeElapsed;
+        }
+
+    }
+}
