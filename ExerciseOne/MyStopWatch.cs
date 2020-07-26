e9118d7c234dfeb8500bc986f95edc3cef6ef5ca
Alan Wrigglesworth <61271711+AlanXVII@users.noreply.github.com>
2020-07-26 21:25:20 +0200

    Update MyStopWatch.cs

--- ExerciseOne/MyStopWatch.cs
+++ ExerciseOne/MyStopWatch.cs
@@ -6,30 +6,71 @@ namespace ExerciseOne
 {
     class MyStopWatch
     {
-        public DateTime StartTime;
-        public DateTime StopTime;
-        public TimeSpan TimeElapsed;
+        private DateTime _startTime;
+        private DateTime _stopTime;
+        private TimeSpan _timeElapsed;
+        private bool _started;
+        private bool _stopped;
 
         public MyStopWatch()
         {
 
         }
-
+       
         public void Start()
         {
-            StartTime = DateTime.Now;
+            if (_started == false)
+            {
+                _startTime = DateTime.Now;
+                _started = true;
+                _stopped = false;
+            }
+            else if (_started == true)
+            {
+                throw new InvalidOperationException("The Stopwatch has already been started");
+            }
         }
 
         public void Stop()
         {
-            StopTime = DateTime.Now;
+            if (_started == true & _stopped == false)
+            {
+                _stopTime = DateTime.Now;
+                _stopped = true;
+                _started = false;
+            }
+            else if (_started == false)
+            {
+                throw new InvalidOperationException("The Stopwatch hasn't been started yet");
+            }
+            
+        }
+
+        public void Reset()
+        {
+            if (_stopped == true)
+            {
+                _stopTime = DateTime.MinValue;
+                _startTime = DateTime.MinValue;
+            }
+            else if (_stopped == false & _started == true)
+            {
+                throw new InvalidOperationException("The StopWatch needs to be Stopped First");
+            }
         }
 
         public TimeSpan Elapsed()
         {
-            TimeElapsed = StopTime - StartTime;
-            return TimeElapsed;
+            if (_stopped == false)
+            {
+                throw new InvalidOperationException("The Stopwatch is still running");
+            }
+            else if (_stopped == true)
+            {
+                _timeElapsed = _stopTime - _startTime;
+                _stopped = false;
+            }
+            return _timeElapsed;
         }
-
     }
 }
