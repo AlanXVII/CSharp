49d171e24cc8940d796dd17e419dae3701934d1e
Alan Wrigglesworth <61271711+AlanXVII@users.noreply.github.com>
2020-07-26 21:30:48 +0200

    Update Program.cs

--- ExerciseOne/Program.cs
+++ ExerciseOne/Program.cs
@@ -7,6 +7,11 @@ namespace ExerciseOne
     {
         static void Main(string[] args)
         {
+
+        }
+
+        public void CallStopwatch()
+        {
             var sw = new MyStopWatch();
             sw.Start();
             Thread.Sleep(1000);
@@ -15,8 +20,13 @@ namespace ExerciseOne
             TimeSpan te = sw.Elapsed();
 
             // Format and display the TimeSpan value.
-            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",te.Hours, te.Minutes, te.Seconds,te.Milliseconds / 10);
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", te.Hours, te.Minutes, te.Seconds, te.Milliseconds / 10);
             Console.WriteLine("RunTime " + elapsedTime);
+        }
+
+        public void CallStackOverFlowPost()
+        {
+
         }
     }
 }
