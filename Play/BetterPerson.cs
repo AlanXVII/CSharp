1f471a1611ad8d872db17e684e61fa61a4b914c1
Alan Wrigglesworth <61271711+AlanXVII@users.noreply.github.com>
2020-07-26 19:50:27 +0200

    Create BetterPerson.cs

---
+++ Play/BetterPerson.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Play
+{
+    class BetterPerson
+    {
+        // auto implemented properties
+        public DateTime BirthDate { get; private set; } //auto implemented property with private set acesser (read-only) 
+        public string Name { get; set; }
+        public string UserName { get; set; }
+
+        //contructors
+        public BetterPerson(DateTime birthDate)
+        {
+            BirthDate = birthDate;
+        }
+        
+        //calculated properties
+        public int Age
+        {
+            get
+            {
+                var timeSpan = DateTime.Today - BirthDate;
+                var years = timeSpan.Days / 365;
+
+                return years;
+            }
+        }
+    }
+}
